96476b78f3b203417fb8572def973618767931e1
BeanHsiang <xiangbin731@qq.com>
2018-06-22 19:30:51 +0800

    added TestBase.

--- test/FacePlusPlus.SDK.Test/FacePPTest.cs
+++ test/FacePlusPlus.SDK.Test/FacePPTest.cs
@@ -1,12 +1,30 @@
+using Microsoft.Extensions.Configuration;
+using System.IO;
 using Xunit;
 
 namespace FacePlusPlus.SDK.Test
 {
-    public class FacePPTest
+    public class TestBase
     {
-        const string API_Key = "";
-        const string API_Secret = "";
+        protected readonly string API_Key = "";
+        protected readonly string API_Secret = "";
 
+        public static IConfigurationRoot Config { get; private set; }
+
+        public TestBase()
+        {
+            var config = new ConfigurationBuilder()
+           .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("app.json")
+           .Build();
+
+            API_Key = config["facepp:API_Key"];
+            API_Secret = config["facepp:API_Secret"];
+        }
+    }
+
+    public class FacePPTest : TestBase
+    {
         [Fact]
         public void TestDetectImageFile()
         {
@@ -24,6 +42,22 @@ namespace FacePlusPlus.SDK.Test
             var response = client.Face_Detect(null, image_url: "https://www.faceplusplus.com.cn/scripts/demoScript/images/demo-pic1.jpg", return_attributes: attrs, return_landmark: 2);
             Assert.NotNull(response.Request_Id);
             Assert.NotEmpty(response.Faces);
+        }
+
+        [Fact]
+        public void TestGetFaceSets()
+        {
+            var client = new APIClient(API_Key, API_Secret);
+            var response = client.FaceSet_GetFaceSets();
+            Assert.NotNull(response.Request_Id);
+        }
+
+        [Fact]
+        public void TestDeleteFaceSets()
+        {
+            var client = new APIClient(API_Key, API_Secret);
+            var response = client.FaceSet_Delete("e990bf901a8ce077230d734af047071a", check_empty: 0);
+            Assert.NotNull(response.Request_Id);
         }
     }
 }
